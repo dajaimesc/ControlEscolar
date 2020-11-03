3063bf9f29d11eb2c62ba8ac9484162eb3065cad
dajaimes <dajaimes@DESKTOP-BPHGM8P>
2020-11-02 18:45:51 -0600

    Corrección de los encabezados del proyecto.

--- src/main/java/com/palmartec/controlescolar/excepciones/LecturaDatosExcepcion.java
+++ src/main/java/com/palmartec/controlescolar/excepciones/LecturaDatosExcepcion.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * UNIVERSIDAD AUTONOMA DE CAMPECHE
+ * PROGRAMACION DE APLICACIONES WEB
+ * 2020
  */
 package com.palmartec.controlescolar.excepciones;
 
